5d29c33856ff3cc91e1433c8d2cd96a92ed6c4aa
Gena <genada89@gmail.com>
2026-08-18 18:11:50 +0300

    change toggle function to POST method

--- todo/views.py
+++ todo/views.py
@@ -26,10 +26,6 @@ class TagDeleteView(generic.DeleteView):
     template_name = "todo/confirm_delete.html"
     success_url = reverse_lazy("todo:tag-list")
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["delete_url"] = self.success_url
-        return context
 
 class IndexView(generic.ListView):
     model = Task
@@ -37,13 +33,13 @@ class IndexView(generic.ListView):
 
 class TaskCreateView(generic.CreateView):
     model = Task
-    fields = "__all__"
+    fields = ("content", "deadline", "tags", "is_completed")
     success_url = reverse_lazy("todo:index")
     template_name = "todo/create_update_form.html"
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
-    fields = "__all__"
+    fields = ("content", "deadline", "tags", "is_completed")
     success_url = reverse_lazy("todo:index")
     template_name = "todo/create_update_form.html"
 
@@ -52,19 +48,12 @@ class TaskDeleteView(generic.DeleteView):
     template_name = "todo/confirm_delete.html"
     success_url = reverse_lazy("todo:index")
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["delete_url"] = self.success_url
-        return context
 
 def toggle_complete(request, pk):
-    task = Task.objects.get(id=pk)
-    if task.is_completed:
-        task.is_completed = False
-    else:
-        task.is_completed = True
-
-    task.save()
+    if request.method == "POST":
+        task = Task.objects.get(id=pk)
+        task.is_completed = not task.is_completed
+        task.save()
     return HttpResponseRedirect(reverse_lazy("todo:index"))
 
 
