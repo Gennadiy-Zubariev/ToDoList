04f6a94d251a96c584f93dab3c674db56760b0ca
Gena <genada89@gmail.com>
2026-08-18 18:43:31 +0300

    add form with validation deadline

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
 
+from todo.forms import TaskForm
 from todo.models import Tag, Task
 
 
@@ -37,14 +38,14 @@ class IndexView(generic.ListView):
 
 class TaskCreateView(generic.CreateView):
     model = Task
-    fields = ("content", "deadline", "tags", "is_completed")
+    form_class = TaskForm
     success_url = reverse_lazy("todo:index")
     template_name = "todo/create_update_form.html"
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
-    fields = ("content", "deadline", "tags", "is_completed")
+    form_class = TaskForm
     success_url = reverse_lazy("todo:index")
     template_name = "todo/create_update_form.html"
 
